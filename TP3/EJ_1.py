b22d1c3163baafadcd8617758298658d19dae554
UADER_IS2_Baez <baezbrenda247@gmail.com>
2026-04-27 19:22:08 -0300

    Tp3 ejercicio 1

---
+++ TP3/EJ_1.py
@@ -0,0 +1,20 @@
+import math
+
+class CalculadorFactorial:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def calcular(self, n):
+        return math.factorial(n)
+
+calc1 = CalculadorFactorial()
+calc2 = CalculadorFactorial()
+
+print(f"¿Son la misma instancia? {calc1 is calc2}")
+print(f"Factorial de 12: {calc1.calcular(12)}")
+print(f"Factorial de 7: {calc2.calcular(7)}")
+print(f"Factorial de 9: {calc1.calcular(9)}")
