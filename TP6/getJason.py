c36b9d8274ebffb46951eb3d36d3f547a7cffb52
UADER_IS2_Baez <baezbrenda247@gmail.com>
2026-05-27 10:27:12 -0300

    TP6 correcciones

---
+++ TP6/getJason.py
@@ -0,0 +1,11 @@
+import json
+import sys
+
+jsonfile = sys.argv[1]
+jsonkey = sys.argv[2] if len(sys.argv) > 2 else "token1"
+
+with open(jsonfile, "r") as myfile:
+    data = myfile.read()
+
+obj = json.loads(data)
+print(str(obj[jsonkey]))
